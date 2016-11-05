e4589db5db027aab6bce3197a75ed1a684917b09
gothmog3479 <gothmog79@mail.ru>
2016-11-06 00:19:09 +0300

    add interface EntityDao

--- src/main/java/ru/gothmog/web/library/dao/EntityDao.java
+++ src/main/java/ru/gothmog/web/library/dao/EntityDao.java
@@ -5,12 +5,12 @@ import java.util.List;
 /**
  * Created by gothmog on 22.09.2016.
  */
-public interface EntityDao<E> {
-    boolean create(E e);
+public interface EntityDao<T> {
+    boolean create(T type);
 
     void delete(long id);
 
-    E read(long id);
+    T read(long id);
 
-    List<E> getAll();
+    List<T> getAll();
 }
