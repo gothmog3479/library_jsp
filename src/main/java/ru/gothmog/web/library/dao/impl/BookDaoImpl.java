2c62069e35cf10037a07e40c4a1289a5b5ccd792
gothmog3479 <gothmog79@mail.ru>
2016-11-07 22:17:40 +0300

    add metod BookByGenre

--- src/main/java/ru/gothmog/web/library/dao/impl/BookDaoImpl.java
+++ src/main/java/ru/gothmog/web/library/dao/impl/BookDaoImpl.java
@@ -6,10 +6,7 @@ import ru.gothmog.web.library.dao.EntityDao;
 import ru.gothmog.web.library.dao.settings.ImplDaoSettings;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -99,16 +96,105 @@ public class BookDaoImpl implements EntityDao<Book> {
     }
     @Override
     public void delete(long id) {
+        log.info("Delete book");
+        String sql = "DELETE FROM library.book WHERE id = ?";
+        try (Connection connection = daoSettings.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
 
+            int rowsDelete = statement.executeUpdate();
+            if (rowsDelete > 0) {
+                log.info("A book was deleted successfully!");
+            }
+        } catch (SQLException | IOException ex) {
+            log.error("Error delete and input book data", ex);
+        }
     }
     @Override
     public Book read(long id) {
-        return null;
+        log.info("Read book");
+        Book book = new Book();
+        String sql = "SELECT * FROM  library.book WHERE id = ?";
+        try (Connection connection = daoSettings.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            book.setId(resultSet.getLong("id"));
+            book.setBookName(resultSet.getString("bookName"));
+            book.setContent(resultSet.getBytes("content"));
+            book.setPageCount(resultSet.getInt("pageCount"));
+            book.setIsbn(resultSet.getString("isbn"));
+            book.setAuthor((Author) resultSet.getObject("author"));
+            book.setGenre((Genre) resultSet.getObject("genre"));
+            book.setPublisher((Publisher) resultSet.getObject("publisher"));
+            book.setPublishDate(resultSet.getDate("publishDate"));
+            book.setImage(resultSet.getBytes("image"));
+            book.setDescription(resultSet.getString("description"));
+            book.setCreateDate(resultSet.getDate("createDate"));
+            book.setLastEditedDate(resultSet.getDate("lastEditedDate"));
+            book.setCreateUserBook((User) resultSet.getObject("createUserBook"));
+            book.setLastEditedUserBook((User) resultSet.getObject("lastEditedUserBook"));
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException | IOException ex) {
+            log.error("Error when reading and input book's data", ex);
+        }
+        return book;
     }
 
     @Override
     public List<Book> getAll() {
-        return null;
+        log.info("Get all books");
+        List<Book> list = new ArrayList<>();
+        String sql = "SELECT b.id,b.bookname,b.isbn,b.pagecount,b.publisherdate, p.fullname AS publisher, " +
+                "a.fullname AS author, g.names AS genre, b.image FROM library.book b " +
+                "INNER JOIN library.author a ON b.authorid = a.id " +
+                "INNER JOIN library.genre g ON b.genreid = g.id " +
+                "INNER JOIN library.publisher p ON b.publisherid = p.id ORDER BY b.bookname";
+        try (Connection connection = daoSettings.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getLong("id"));
+                book.setBookName(resultSet.getString("bookName"));
+                book.setContent(resultSet.getBytes("content"));
+                book.setPageCount(resultSet.getInt("pageCount"));
+                book.setIsbn(resultSet.getString("isbn"));
+                book.setAuthor((Author) resultSet.getObject("author"));
+                book.setGenre((Genre) resultSet.getObject("genre"));
+                book.setPublisher((Publisher) resultSet.getObject("publisher"));
+                book.setPublishDate(resultSet.getDate("publishDate"));
+                book.setImage(resultSet.getBytes("image"));
+                book.setDescription(resultSet.getString("description"));
+                book.setCreateDate(resultSet.getDate("createDate"));
+                book.setLastEditedDate(resultSet.getDate("lastEditedDate"));
+                book.setCreateUserBook((User) resultSet.getObject("createUserBook"));
+                book.setLastEditedUserBook((User) resultSet.getObject("lastEditedUserBook"));
+                list.add(book);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException | IOException ex) {
+            log.error("Error when getting and input output all books");
+        }
+        return list;
+    }
+
+    public List<Book> getBooksByGenre(long id){
+        log.info("Get books by genre");
+        List<Book> list = new ArrayList<>();
+
+        String sql = "SELECT b.id,b.bookname,b.isbn,b.pagecount,b.publisherdate, p.fullname AS publisher, " +
+                "a.fullname AS author, g.names AS genre, b.image FROM library.book b " +
+                "INNER JOIN library.author a ON b.authorid = a.id " +
+                "INNER JOIN library.genre g ON b.genreid = g.id " +
+                "INNER JOIN library.publisher p ON b.publisherid = p.id " +
+                "WHERE genreid = " + id + " ORDER BY b.bookname LIMIT 0 OFFSET 5";
+        return  list;
     }
 
     public List<Book> getBookList() {
