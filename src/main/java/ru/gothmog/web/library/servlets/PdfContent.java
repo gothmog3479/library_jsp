086fad46acabdf6388131d592aadceb068232820
gothmog3479 <gothmog79@mail.ru>
2016-11-11 17:16:07 +0300

    add PdfContent

--- src/main/java/ru/gothmog/web/library/servlets/PdfContent.java
+++ src/main/java/ru/gothmog/web/library/servlets/PdfContent.java
@@ -1,10 +1,42 @@
-@javax.servlet.annotation.WebServlet(name = "PdfContent")
-public class PdfContent extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package ru.gothmog.web.library.servlets;
+
+import org.apache.log4j.Logger;
+import ru.gothmog.web.library.beans.Book;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+
+public class PdfContent extends HttpServlet {
+    private static final Logger log = Logger.getLogger(PdfContent.class);
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("application/pdf");
+        OutputStream out = response.getOutputStream();
+        try {
+            int index = Integer.valueOf(request.getParameter("index"));
+            ArrayList<Book> list = (ArrayList<Book>) request.getSession(false).getAttribute("currentBookList");
+            Book book = list.get(index);
+            book.fillPdfContent();
+            response.setContentLength(book.getContent().length);
+            out.write(book.getContent());
+        } catch (Exception ex) {
+           log.error("Error request" + ex);
+        } finally {
+            out.close();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 }
