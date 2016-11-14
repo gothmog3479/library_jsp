ab217f5d9e57ad489f4ab82192db1bc1ff154f2b
gothmog3479 <gothmog79@mail.ru>
2016-11-14 21:32:30 +0300

    add LetterList

---
+++ src/main/java/ru/gothmog/web/library/beans/LetterList.java
@@ -0,0 +1,7 @@
+package ru.gothmog.web.library.beans;
+
+/**
+ * Created by gothmog on 14.11.2016.
+ */
+public class LetterList {
+}
---
+++ src/main/java/ru/gothmog/web/library/servlets/ShowImage.java
@@ -0,0 +1,7 @@
+package ru.gothmog.web.library.servlets;
+
+/**
+ * Created by gothmog on 14.11.2016.
+ */
+public class ShowImage {
+}
