e910c3945b42d6f1d3d44e727abe7c68620e6904
gothmog3479 <gothmog79@mail.ru>
2016-11-14 21:33:59 +0300

    add ShowImage

--- src/main/java/ru/gothmog/web/library/servlets/ShowImage.java
+++ src/main/java/ru/gothmog/web/library/servlets/ShowImage.java
@@ -1,7 +1,52 @@
 package ru.gothmog.web.library.servlets;
 
+import ru.gothmog.web.library.beans.Book;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
 /**
  * Created by gothmog on 14.11.2016.
  */
-public class ShowImage {
+public class ShowImage extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("image/jpeg");
+        OutputStream out = response.getOutputStream();
+        try {
+            int index = Integer.valueOf(request.getParameter("index"));
+
+            ArrayList<Book> list = (ArrayList<Book>)request.getSession(false).getAttribute("currentBookList");
+            Book book = list.get(index);
+            response.setContentLength(book.getImage().length);
+            out.write(book.getImage());
+        }catch (Exception ex){
+            ex.printStackTrace();
+        } finally {
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
 }
