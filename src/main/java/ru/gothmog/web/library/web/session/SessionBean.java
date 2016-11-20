f65fc29d5d0d88e02c9c408dc98edcc185cd5802
gothmog3479 <gothmog79@mail.ru>
2016-11-20 03:15:08 +0300

    add SessionBean

--- src/main/java/ru/gothmog/web/library/web/session/SessionBean.java
+++ src/main/java/ru/gothmog/web/library/web/session/SessionBean.java
@@ -1,7 +1,20 @@
 package ru.gothmog.web.library.web.session;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by gothmog on 19.11.2016.
  */
 public class SessionBean {
+    public static HttpSession getSession() {
+        return (HttpSession) FacesContext.getCurrentInstance()
+                .getExternalContext().getSession(false);
+    }
+
+    public static HttpServletRequest getRequest() {
+        return (HttpServletRequest) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequest();
+    }
 }
