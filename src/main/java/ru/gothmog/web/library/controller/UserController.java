80db1211b593c494c2ed5e6ac145822b6ff8cff7
gothmog3479 <gothmog79@mail.ru>
2016-11-20 03:15:42 +0300

    update UserController

--- src/main/java/ru/gothmog/web/library/controller/UserController.java
+++ src/main/java/ru/gothmog/web/library/controller/UserController.java
@@ -1,7 +1,35 @@
 package ru.gothmog.web.library.controller;
 
+import ru.gothmog.web.library.beans.User;
+import ru.gothmog.web.library.dao.impl.UserDaoImpl;
+import ru.gothmog.web.library.web.session.SessionBean;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gothmog on 19.11.2016.
  */
 public class UserController {
+    private User user = new User();
+    private List<User> userArrayList = new ArrayList<>();
+    private UserDaoImpl userDao = new UserDaoImpl();
+
+    public List<User> getUserArrayList(){
+
+        userArrayList = userDao.getAll();
+        return userArrayList;
+    }
+
+    public void setUserArrayList(List<User> userArrayList) {
+        this.userArrayList = userArrayList;
+    }
+
+//    public String createUser(){
+//        boolean createUser = userDao.create(user);
+//        if (createUser){
+//            HttpSession session = SessionBean.getSession();
+//        }
+//    }
 }
